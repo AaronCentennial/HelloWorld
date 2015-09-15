18a48559a2d7c825003bfde292fb96322d08aa2b
AaronCentennial <aferna95@my.centennialcollege.ca>
2015-09-15 11:16:13 -0400

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+  public static void main(String[] args) {
+		String name="Aaron Fernandes";
+		System.out.printf("Hello World %s", name);
+  }
+}
---
+++ src/com/company/Program.java
@@ -0,0 +1,8 @@
+package com.company;
+
+/**
+ * Created by aaron on 15/09/15.
+ */
+public class Program {
+	
+}
