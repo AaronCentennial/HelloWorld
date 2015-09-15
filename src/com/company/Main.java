6fa9e7495fd2eabe89bb9eadb7f980217921cab5
AaronCentennial <aferna95@my.centennialcollege.ca>
2015-09-15 11:32:41 -0400

    added new class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,6 @@ package com.company;
 
 public class Main {
   public static void main(String[] args) {
-		String name="Aaron Fernandes";
-		System.out.printf("Hello World %s", name);
+		System.out.printf("Hello World %s", Program.printName());
   }
 }
--- src/com/company/Program.java
+++ src/com/company/Program.java
@@ -4,5 +4,7 @@ package com.company;
  * Created by aaron on 15/09/15.
  */
 public class Program {
-	
+	public static String printName(){
+		return "Aaron Fernandes";
+	}
 }
