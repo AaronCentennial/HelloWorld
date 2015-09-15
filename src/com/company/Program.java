09732e6e5fe7fa69cf0ab1b57bd442f2fabf1099
AaronCentennial <aferna95@my.centennialcollege.ca>
2015-09-15 12:22:31 -0400

    added private vars getters and setters

--- src/com/company/Program.java
+++ src/com/company/Program.java
@@ -1,10 +1,44 @@
 package com.company;
 
 /**
- * Created by aaron on 15/09/15.
+ * Created by Aaron Fernandes on 15/09/15.
  */
 public class Program {
-	public static String printName(){
-		return "Aaron Fernandes";
+	private String _name;
+	private String _gender;
+	private int _age;
+
+	//getter for name
+	public String getName(){
+		return this._name;
 	}
+
+	//setter for name
+	public void setName(String name){
+		this._name=name;
+	}
+
+	//getter for gender
+	public String getGender(){
+		return this._gender;
+	}
+
+	//setter for gender
+	public void setGender(String gender){
+		this._gender=gender;
+	}
+
+	/*
+		Constructor
+		@param age of person
+	 */
+	Program(int age){
+		this._age=age;
+	}
+
+	public void runs(){
+		System.out.printf("%s who is %d runs", this._age, this._name);
+	}
+
+
 }
